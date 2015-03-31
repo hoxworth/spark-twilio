1ee95080292d9e71750ee7fd3a047711775f41e9
David Middlecamp <david@spark.io>
2015-03-31 11:52:40 -0500

    refactoring a bit to work with more explicit hooks

--- twilio.cpp
+++ twilio.cpp
@@ -1,37 +1,62 @@
 #include "twilio.h"
+#include "application.h"
 
-// Need account sid, auth token, from number?
-Twilio::Twilio(char *aSid, char *aToken) {
-    accountSid = new String(aSid);
-    authToken = new String(aToken);
+Twilio::Twilio() {
+
 }
+
+// This will work better when the maximum publish size limits are increased,
+// in the meantime, be sure to store ACCT and TOKEN in your webhook definiton
+// Twilio::Twilio(char *aSid, char *aToken) {
+//     accountSid = new String(aSid);
+//     authToken = new String(aToken);
+// }
 
 Twilio::~Twilio() {
     delete accountSid;
     delete authToken;
 }
 
-void Twilio::message(char *toNumber, char *fromNumber, char *body) {
+void Twilio::message(const char *body) {
+    message(NULL, NULL, body, NULL);
+}
+
+void Twilio::message(const char *toNumber, const char *fromNumber, const char *body) {
     message(toNumber, fromNumber, body, "");
 }
 
-void Twilio::message(char *toNumber, char *fromNumber, char *body, char * mediaUrl) {
-    String json = String("{\"ACCT\":\"");
+void Twilio::message(const char *toNumber, const char *fromNumber, const char *body, const char *mediaUrl) {
     String escapedBody = String(body);
     escapedBody.replace("\"", "\\\"");
 
-    json.concat(*accountSid);
-    json.concat(String("\",\"TOKEN\":\""));
-    json.concat(*authToken);
-    json.concat(String("\",\"TO\":\""));
-    json.concat(String(toNumber));
-    json.concat(String("\",\"FROM\":\""));
-    json.concat(String(fromNumber));
-    json.concat(String("\",\"BODY\":\""));
-    json.concat(String(body));
-    json.concat(String("\",\"MEDIA\":\""));
-    json.concat(String(mediaUrl));
-    json.concat(String("\"}"));
-    
+    String json = String("{"); 
+
+
+    json.concat(String("\"BODY\":\"") + String(escapedBody)+ String("\""));
+
+    //TODO: uncomment when max publish length is increased
+    // json.concat(String("\"TO\":\"") + String(toNumber)+ String("\","));    
+    //
+    // json.concat(String("\"FROM\":\"") + String(fromNumber)+ String("\","));
+    //
+    // if (this->accountSid != NULL) {
+    //     json.concat(String(",\"ACCT\":\"") + String(*this->accountSid) + String("\""));
+    // }
+    //
+    // if (this->authToken != NULL) {
+    //     json.concat(String(",\"TOKEN\":\"") + String(*this->authToken) + String("\","));
+    // }
+    //
+    // if (mediaUrl != NULL) {
+    //     json.concat(String(",\"MEDIA\":\"") + String(mediaUrl) + String("\""));
+    // }
+
+    json.concat("}");
+
+    // make sure you're not calling this more than once per second,
+    // and checkout Twilio pricing here: 
+    //  https://www.twilio.com/user/account/usage/primary
+    //
     Spark.publish("Twilio/message", json.c_str(), 60, PRIVATE);
 }
+
